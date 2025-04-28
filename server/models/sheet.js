59aab04694ba4802cd6ea5d827e03e51360d6a58
sj11105 <snehajain11105@gmail.com>
2025-04-28 18:07:42 +0530

    fix: move files to src for Render

--- server/models/sheet.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const SheetSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  imageData: String, // base64 PNG string
-  createdAt: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model("Sheet", SheetSchema);
